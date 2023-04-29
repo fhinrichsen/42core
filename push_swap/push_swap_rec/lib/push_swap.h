7eccea8ae0b22349ec845d896861a894357e82b7
fhinrich <finnrichsen@gmail.com>
2023-04-29 16:52:25 +0200

    Changed makefile

---
+++ push_swap/push_swap_rec/lib/push_swap.h
@@ -0,0 +1,10 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdio.h>
+//put your function headers here
+
+//errors
+
+
+#endif
---
+++ push_swap/push_swap_rec/src/main.c
@@ -0,0 +1,7 @@
+#include "../lib/push_swap.h"
+
+int main(void)
+{
+	printf("Hello world");
+	return(0);
+}
